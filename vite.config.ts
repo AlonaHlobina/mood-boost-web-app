f4ed3aa00ab589ef149cbe02bb5a63a057485e24
AlonaHlobina <54394529+AlonaHlobina@users.noreply.github.com>
2025-08-29 13:35:01 +0000

    Edited Spark

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/mood-boost-web-app/' : '/',
   plugins: [
     react(),
     tailwindcss(),
